cf4bdb5b8b87e417f8d4365f5f4b0e3fcaaacc98
Parkoba <parkobaofficial@gmail.com>
2023-04-08 14:52:49 +0100

    commit

---
+++ js/booking.js
@@ -0,0 +1,8 @@
+// const image = document.querySelector('img');
+
+// function resizeImage() {
+//   const containerWidth = document.querySelector('.container').offsetWidth;
+//   image.style.width = `${containerWidth / 2}px`;
+// }
+
+// window.addEventListener('resize', resizeImage);
