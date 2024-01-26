24bf1293a33dcb07b5f8a4d6a60dbe3364a947ea
Alonge Oluwatobiloba <127941765+Parkoba@users.noreply.github.com>
2024-01-26 14:19:34 +0100

    Full code commit

--- js/booking.js
+++
@@ -1,8 +0,0 @@
-// const image = document.querySelector('img');
-
-// function resizeImage() {
-//   const containerWidth = document.querySelector('.container').offsetWidth;
-//   image.style.width = `${containerWidth / 2}px`;
-// }
-
-// window.addEventListener('resize', resizeImage);
